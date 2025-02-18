ad6893e9ae2a8a12b5381cdc1576205830416819
Piers <piersosborne225@fastmail.com>
2025-02-18 21:34:32 +0000

    WIP

--- VectorOperationsBenchmark.cs
+++ VectorOperationsBenchmark.cs
@@ -20,8 +20,12 @@ public class VectorOperationsBenchmark
 
     private Vector3d v1_Manual;
     private Vector3d v2_Manual;
+    private Vector3d v3_Manual;
+    private Vector3d v4_Manual;
     private Vector<double> v1_SIMD;
     private Vector<double> v2_SIMD;
+    private Vector<double> v3_SIMD;
+    private Vector<double> v4_SIMD;
     private static readonly Random random = new Random();
 
     [GlobalSetup]
@@ -29,21 +33,27 @@ public class VectorOperationsBenchmark
     {
         v1_Manual = new Vector3d(random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0);
         v2_Manual = new Vector3d(random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0);
+        v3_Manual = new Vector3d(random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0);
+        v4_Manual = new Vector3d(random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0);
 
         v1_SIMD = new Vector<double>(new double[] { random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0 });
         v2_SIMD = new Vector<double>(new double[] { random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0 });
+        v3_SIMD = new Vector<double>(new double[] { random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0 });
+        v4_SIMD = new Vector<double>(new double[] { random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0, random.NextDouble() * 10.0 });
     }
 
     [Benchmark]
-    public Vector3d ManualAddition()
+    public Vector3d ManualArithmetic()
     {
-        return new Vector3d(v1_Manual.X + v2_Manual.X, v1_Manual.Y + v2_Manual.Y, v1_Manual.Z + v2_Manual.Z);
+        return new Vector3d((v1_Manual.X + v2_Manual.X) * v3_Manual.X / v4_Manual.X, 
+                        (v1_Manual.Y + v2_Manual.Y) * v3_Manual.Y / v4_Manual.Y, 
+                        (v1_Manual.Z + v2_Manual.Z) * v3_Manual.Z / v4_Manual.Z);
     }
 
     [Benchmark]
-    public Vector<double> SIMDAddition()
+    public Vector<double> SIMDArithmetic()
     {
-        return v1_SIMD + v2_SIMD;
+        return (v1_SIMD + v2_SIMD) * v3_SIMD / v4_SIMD;
     }
 
     [Benchmark]
@@ -55,49 +65,54 @@ public class VectorOperationsBenchmark
     [Benchmark]
     public double SIMDDotProduct()
     {
-        return Vector.Dot(v1_SIMD, v2_SIMD);
+        return Vector.Dot(v1_SIMD, v2_SIMD) + Vector.Dot(v3_SIMD, v4_SIMD);
     }
 
     [Benchmark]
     public Vector3d ManualMin()
     {
         return new Vector3d(
-            v1_Manual.X < v2_Manual.X ? v1_Manual.X : v2_Manual.X,
-            v1_Manual.Y < v2_Manual.Y ? v1_Manual.Y : v2_Manual.Y,
-            v1_Manual.Z < v2_Manual.Z ? v1_Manual.Z : v2_Manual.Z
+            Math.Min(Math.Min(Math.Min(v1_Manual.X, v2_Manual.X), v3_Manual.X), v4_Manual.X),
+            Math.Min(Math.Min(Math.Min(v1_Manual.Y, v2_Manual.Y), v3_Manual.Y), v4_Manual.Y),
+            Math.Min(Math.Min(Math.Min(v1_Manual.Z, v2_Manual.Z), v3_Manual.Z), v4_Manual.Z)
         );
     }
 
     [Benchmark]
     public Vector<double> SIMDMin()
     {
-        return Vector.Min(v1_SIMD, v2_SIMD);
+        return Vector.Min(Vector.Min(Vector.Min(v1_SIMD, v2_SIMD), v3_SIMD), v4_SIMD);
     }
 
     [Benchmark]
-    public double ManualSumBoth()
+    public double ManualSumAll()
     {
         return v1_Manual.X + v1_Manual.Y + v1_Manual.Z + 
-                v2_Manual.X + v2_Manual.Y + v2_Manual.Z;
+                v2_Manual.X + v2_Manual.Y + v2_Manual.Z +
+                v3_Manual.X + v3_Manual.Y + v3_Manual.Z +
+                v4_Manual.X + v4_Manual.Y + v4_Manual.Z;
     }
 
     [Benchmark]
-    public double SIMDSumBoth()
+    public double SIMDSumAll()
     {
-        return Vector.Sum(v1_SIMD) + Vector.Sum(v2_SIMD);
+        return Vector.Sum(v1_SIMD) + Vector.Sum(v2_SIMD) + Vector.Sum(v3_SIMD) + Vector.Sum(v4_SIMD);
     }
 
     [Benchmark]
     public bool ManualLessThanOrEqualAll()
     {
-        return v1_Manual.X < v2_Manual.X && 
-                v1_Manual.Y < v2_Manual.Y && 
-                v1_Manual.Z < v2_Manual.Z; 
+        return (v1_Manual.X <= v2_Manual.X && 
+                v1_Manual.Y <= v2_Manual.Y && 
+                v1_Manual.Z <= v2_Manual.Z) ||
+                (v3_Manual.X >= v4_Manual.X && 
+                v3_Manual.Y >= v4_Manual.Y && 
+                v3_Manual.Z >= v4_Manual.Z); 
     }
 
     [Benchmark]
     public bool SIMDLessThanOrEqualAll()
     {
-        return Vector.LessThanOrEqualAll(v1_SIMD, v2_SIMD);
+        return Vector.LessThanOrEqualAll(v1_SIMD, v2_SIMD) || Vector.GreaterThanOrEqualAll(v3_SIMD, v4_SIMD);
     }
 }
